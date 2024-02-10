58e89dcce188c343268ec12a1387abd7d4d5155b
OmDutaa <85216552+DutaKey@users.noreply.github.com>
2024-02-10 10:58:57 +0700

    fix: username not showing

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -14,7 +14,9 @@
                     <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><img
                             class="mr-2 w-6 h-6 rounded-full"
                             src="{{ asset($comment->user->user_profile) ?? asset('guest.png') }}"
-                            alt="{{ $comment->user->username }}">{{ Str::ucfirst($comment->user->name) }}</p>
+                            alt="{{ $comment->user->username }}">{{ Str::ucfirst($comment->user->username) }}
+                        <livewire:component.verified :user="$comment->user" />
+                    </p>
                     <p class="text-sm text-gray-600 dark:text-gray-400">
                         <time pubdate datetime="{{ $comment->presenter()->relativeCreatedAt() }}"
                             title="{{ $comment->presenter()->relativeCreatedAt() }}">
